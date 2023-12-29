d74f6975d71936803e47fcd55371648067e2866a
Freddy23245 <cyberdyne710@gmail.com>
2023-12-29 15:56:15 -0300

    correccion

--- AbmAPI/AbmAPI/obj/Debug/net6.0/AbmAPI.AssemblyInfo.cs
+++ AbmAPI/AbmAPI/obj/Debug/net6.0/AbmAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AbmAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f30d9a7b26b009a782b0469dbced992e93afbb54")]
 [assembly: System.Reflection.AssemblyProductAttribute("AbmAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AbmAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
